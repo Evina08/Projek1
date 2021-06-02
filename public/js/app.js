240daf68dbfbd353caaedc8eaecef88f24cb1cfb
= <=>
2021-06-02 09:29:46 +0700

    add field baru

--- public/js/app.js
+++ public/js/app.js
@@ -37423,8 +37423,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\KULIAH\SEMESTER 3\github\Projek1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\KULIAH\SEMESTER 3\github\Projek1\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Polinema\SEMESTER 4\Proyek1\Projek1\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Polinema\SEMESTER 4\Proyek1\Projek1\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
